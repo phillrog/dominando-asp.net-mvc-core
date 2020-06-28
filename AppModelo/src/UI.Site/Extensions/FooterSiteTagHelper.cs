bfe4b53f748a49d14ada886447f52fdf9c46bf7a
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-28 13:42:59 -0300

    Foi criado o footer-site para exibir o ano da aplicação

---
+++ AppModelo/src/UI.Site/Extensions/FooterSiteTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.Site.Extensions
+{
+	public class FooterSiteTagHelper : TagHelper
+	{
+		public async override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "p";
+			var content = await output.GetChildContentAsync();
+			var target = content.GetContent() + $"Copyright @@ {@DateTime.Now.Year}";
+			output.Content.SetHtmlContent(target);
+		}
+	}
+}
