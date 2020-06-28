3f4dec124163ef4b79f5ceb22e0ced76c5911a34
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-28 15:36:13 -0300

    Melhorando a area com rotas

--- AppModelo/src/UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
+++ AppModelo/src/UI.Site/Modulos/Produtos/Controllers/CadastroController.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace UI.Site.Areas.Produtos.Controllers
+namespace UI.Site.Modulos.Produtos.Controllers
 {
 	[Area("Produtos")]
+	[Route("produtos")]
     public class CadastroController : Controller
     {
+		[Route("lista")]
         public IActionResult Index()
         {
             return View();
         }
-    }
+
+		[Route("busca")]
+		public IActionResult Busca()
+		{
+			return View();
+		}
+	}
 }
