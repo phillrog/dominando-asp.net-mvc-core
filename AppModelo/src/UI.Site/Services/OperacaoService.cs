bc9d93299f69c15a0fd5db37f206c2b7e9e7ff13
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-28 16:31:19 -0300

    Foi criado as classes de serviço

---
+++ AppModelo/src/UI.Site/Services/OperacaoService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.Site.Services
+{
+	public class OperacaoService
+	{
+		public IOperacaoTransient Transient { get; }
+		public IOperacaoScoped Scoped { get; }
+		public IOperacaoSingleton Singleton { get; }
+		public IOperacaoSingletonInstance SingletonInstance { get; }
+
+		public OperacaoService(IOperacaoTransient transient, IOperacaoScoped scoped , IOperacaoSingleton singleton, IOperacaoSingletonInstance singletonInstance)
+		{
+			Transient = transient;
+			Scoped = scoped;
+			Singleton = singleton;
+			SingletonInstance = singletonInstance;
+		}
+
+			
+	}
+
+	public class Operacao : IOperacaoTransient, IOperacaoScoped, IOperacaoSingleton, IOperacaoSingletonInstance
+	{
+		public Operacao() : this(Guid.NewGuid())
+		{
+
+		}
+
+		public Operacao(Guid id)
+		{
+			OperacaoId = id;
+		}
+
+		public Guid OperacaoId { get; private set; }
+	}
+
+	public interface IOperacaoTransient:IOperacao
+	{
+	}
+
+	public interface IOperacao
+	{
+		Guid OperacaoId { get; }
+	}
+	public interface IOperacaoScoped : IOperacao
+	{
+	}
+	public interface IOperacaoSingleton : IOperacao
+	{
+	}
+	public interface IOperacaoSingletonInstance : IOperacao
+	{
+	}
+}
