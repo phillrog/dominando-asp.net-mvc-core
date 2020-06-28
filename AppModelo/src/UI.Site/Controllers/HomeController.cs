3d57868c20ba1cb0b22db28e13b407c320ac7d8b
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-28 16:11:23 -0300

    APlicando a injeção de dependencia no controller e dentro da view

--- AppModelo/src/UI.Site/Controllers/HomeController.cs
+++ AppModelo/src/UI.Site/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UI.Site.Data;
 
 namespace UI.Site.Controllers
 {
     public class HomeController : Controller
     {
+		private readonly IPedidoRespository _pedidoRepository;
+
+		public HomeController(IPedidoRespository pedidoRepository)
+		{
+			_pedidoRepository = pedidoRepository;
+		}
         public IActionResult Index()
         {
-            return View();
+			var pedido = _pedidoRepository.ObeterPedido();
+
+			return View(pedido);
         }
     }
 }
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Curso Asp .Net MVC Core do desenvolvedor.io
 - Foi aprendido a utilizar TagHelpers
 - Foi aprendido a utilizar áreas para separar estruturas MVC
 - Foi entendido melhor o recurso de áreas e como melhorar suas nomenclaturas com atributos de rotas
+- Foi entendido e aplicado o conceito de injeção de dependencia
+- Foi aprendido a utilizar a injeção de dependencia dentro de uma view
