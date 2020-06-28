0bfc3ef2bd54d0e6b01cfb15c008861f107ad752
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-28 15:29:55 -0300

    COnfigurando a area manualmente

--- AppModelo/src/UI.Site/Startup.cs
+++ AppModelo/src/UI.Site/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -17,6 +18,13 @@ namespace UI.Site
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.Configure<RazorViewEngineOptions>(options => {
+				options.AreaViewLocationFormats.Clear();
+				options.AreaViewLocationFormats.Add(item: "/Modulos/{2}/Views/{1}/{0}.cshtml");
+				options.AreaViewLocationFormats.Add(item: "/Modulos/{2}/Views/Shared/{0}.cshtml");
+				options.AreaViewLocationFormats.Add(item: "/Views/Shared/{0}.cshtml");
+			});
+
 			services.AddMvc(option => option.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 		}
 
