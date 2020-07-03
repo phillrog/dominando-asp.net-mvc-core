a3c6813227ea05d7f11731c897cc4de8f604b318
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-07-03 12:38:01 -0300

    Entendendo autenticação com Identity

--- AppModelo/AspNetCoreIdentity/Controllers/HomeController.cs
+++ AppModelo/AspNetCoreIdentity/Controllers/HomeController.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AspNetCoreIdentity.Controllers
 {
+	[Authorize]
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
@@ -18,11 +20,13 @@ namespace AspNetCoreIdentity.Controllers
 			_logger = logger;
 		}
 
+		[AllowAnonymous]
 		public IActionResult Index()
 		{
 			return View();
 		}
 
+		//[Authorize]
 		public IActionResult Privacy()
 		{
 			return View();
